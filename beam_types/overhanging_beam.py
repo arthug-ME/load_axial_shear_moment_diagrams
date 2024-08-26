eca3d81bb01e762998edb6d0279b0532a68310a6
arthug-ME <arthurgim12@gmail.com>
2024-08-26 11:14:29 -0700

    Flipped the moment units so that it is length*force instead of force*length

--- beam_types/overhanging_beam.py
+++ beam_types/overhanging_beam.py
@@ -601,7 +601,7 @@ def moment_diagram(ax, inputted_length, total_v_forces, moments, v_forces, dist_
         location = force['location']
         magnitude = force['magnitude']
         plt.axvline(x=location, linestyle='--',
-                    label=f'Shear Force at {location} {length_unit}, {magnitude} {force_unit}*{length_unit}')
+                    label=f'Shear Force at {location} {length_unit}, {magnitude} {length_unit}*{force_unit}')
 
     # This plots a vertical line for the start and end of distributed loads
     for load in dist_loads:
@@ -618,7 +618,7 @@ def moment_diagram(ax, inputted_length, total_v_forces, moments, v_forces, dist_
         location = moment['location']
         magnitude = moment['magnitude']
         plt.axvline(x=location, linestyle='--', color='red',
-                    label=f'Moment at {location} {length_unit}, {magnitude} {force_unit}*{length_unit}')
+                    label=f'Moment at {location} {length_unit}, {magnitude} {length_unit}*{force_unit}')
 
     # Find the maximum absolute value and its corresponding x value
     max_index = np.argmax(np.abs(moment_values))
@@ -642,7 +642,7 @@ def moment_diagram(ax, inputted_length, total_v_forces, moments, v_forces, dist_
         xytext = (max_x, max_y * 1.5)
 
     # Label the maximum absolute value
-    ax.annotate(f'Max |Moment|: {abs(max_y):.2f} {force_unit}*{length_unit}\nat x = {max_x:.2f} {length_unit}',
+    ax.annotate(f'Max |Moment|: {abs(max_y):.2f} {length_unit}*{force_unit}\nat x = {max_x:.2f} {length_unit}',
                 xy=(max_x, max_y),
                 xytext=xytext,
                 arrowprops=dict(facecolor='red', shrink=0.05),
@@ -654,7 +654,7 @@ def moment_diagram(ax, inputted_length, total_v_forces, moments, v_forces, dist_
     ax.plot(x_values, moment_values, label="Moment Diagram", color='g')
     ax.set_title("Moment Diagram")
     ax.set_xlabel(f"Position ({length_unit})")
-    ax.set_ylabel(f"Moment ({force_unit}*{length_unit})")
+    ax.set_ylabel(f"Moment ({length_unit}*{force_unit})")
     ax.legend()
     ax.grid(True)
 
